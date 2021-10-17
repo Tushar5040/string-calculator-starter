962295679e58541959abc70f9399445db9213f95
tushar <salunkhe.tushar5040@gmail.com>
2021-10-17 13:03:31 +0530

    method To handle the multiple numbers in the string

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,10 +1,76 @@
 package calculator;
 
+import java.util.Scanner;
+
 class StringCalculator {
 
-    public int add(String input) {
-        return 1;
-      
-    }
+	public int add(String inputString) throws Exception {
+		String[] arrOfNumber;
+		int result = 0;
+		if (inputString.isEmpty()) {
+			return 0;
+		} else {
+			if (inputString.length() > 0) {
+				if (inputString.length() == 1) {
+					try {
+						int num = Integer.parseInt(inputString);
+						return num;
+					} catch (Exception e) {
+
+						System.out.println("string is not numeric");
+
+					}
+
+				} else {
+
+					arrOfNumber = inputString.split(",");
+
+					int negvallcount = 0;
+					for (String Number : arrOfNumber) {
+
+						if (Integer.parseInt(Number) > 1000) {
+							Number = String.valueOf(0);
+
+						} else if (Integer.parseInt(Number) < 0 && negvallcount >= 0) {
+
+							negvallcount++;
+
+						}
+
+						result = result + Integer.parseInt(Number);
+
+					}
+
+					if (negvallcount >= 1) {
+
+						throw (new Exception(
+								"Exception string is less than 0 please input sting with positive numbers"));
+					}
+					return result;
+				}
+			}
+
+			return 1;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("plz input string of Number");
+
+		String stringOfNumbers = scan.nextLine();
+		StringCalculator calculator = new StringCalculator();
+
+		try {
+			int result = calculator.add(stringOfNumbers);
+			System.out.println("result " + result);
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+
+	}
 
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -7,18 +7,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class StringCalculatorShould {
 
     @Test
-    void empty_string_should_return_0() {
+    void empty_string_should_return_0() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
-    void string_with_single_number_should_return_number_as_int() {
+    void string_with_single_number_should_return_number_as_int() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
     @Test
-    void string_with_multiple_number_should_return_number_as_int() {
+    void string_with_multiple_number_should_return_number_as_int() throws Exception {
     	StringCalculator stringCalculator = new StringCalculator();
         assertEquals(21, stringCalculator.add("1,2,3,4,5,6"));
     	
